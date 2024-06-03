8b037ee1045eda20819654b9acc03e6c6acb6983
sashdc <saharshdc@gmail.com>
2024-06-03 14:14:36 -0400

    added homepage with logo and custom font

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
 import Image from "next/image";
+import logo from "../public/images/lb-logo.png";
+import Link from "next/link";
+import { Life_Savers } from "next/font/google";
+
+const lifeSavers = Life_Savers({ subsets: ["latin"], weight: ['400', '700', '800'] });
 
 export default function Home() {
   return (
-    <div className="bg-red-500">Hello world</div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-950 to-gray-500">
+      <Link href="/home" className="text-center">
+        <div className="cursor-pointer">
+          <Image src={logo} alt="Lightbulb logo" className="mx-auto " />
+          <div className="text-center">
+            <h1 className={lifeSavers.className}>Lightbulb</h1>
+            <p className="text-lg font-mono">
+              The posing guide for photographers
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  // other Next.js config options here
+};
+
+export default withPWA({
+  dest: 'public',
+//   disable: process.env.NODE_ENV === 'development', // disable PWA in development
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/], // avoid middleware manifest in service worker
+})(nextConfig);
