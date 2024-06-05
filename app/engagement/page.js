de76111eac62d500703cd04d85eb716e7d280502
sashdc <saharshdc@gmail.com>
2024-06-05 15:09:59 -0400

    added album and lightbox and maps on js file to create album/lightbox

--- app/engagement/page.js
+++ app/engagement/page.js
@@ -6,31 +6,46 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import { engagementPhotos } from "@/public/images/engagement/engagement";
 import Link from "next/link";
+import "yet-another-react-lightbox/plugins/captions.css";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import {  dosis } from "../fonts";
+
 
 export default function EngagementGallery() {
   const [index, setIndex] = React.useState(-1);
 
-  const slides = engagementPhotos.map(photo => ({ src: photo.src }));
+  const slides = engagementPhotos.map((photo) => ({
+    src: photo.src,
+    // title: photo.title,
+    description: photo.description,
+  }));
 
   return (
-    <div className=" ">
-      <PhotoAlbum
-        layout="columns"
-        photos={engagementPhotos}
-        size = "10px"
-        targetRowHeight={150}
-        onClick={({ index: current }) => setIndex(current)}
-      />
+    <div className=" flex flex-col items-center justify-center  p-10  ">
+        <h1 className={`${dosis.className} text-4xl font-bold text-center bg-black/20 rounded-t-xl px-4 pt-1 `}>Engagement Poses</h1>
+      <div className=" p-10 animate-in fade-in duration-1000 bg-black/20 rounded-xl ">
+        <PhotoAlbum
+          layout="columns"
+          photos={engagementPhotos}
+          targetRowHeight={150}
+          onClick={({ index: current }) => setIndex(current)}
+        />
 
-      <Lightbox
-        index={index}
-        slides={slides}
-        open={index >= 0}
-        close={() => setIndex(-1)}
-      />
-
-    <Link href="/poses">
-          <button className="text-blue-500 text-2xl font-bold">Back</button>
+        <Lightbox
+          index={index}
+          plugins={[Captions]}
+          descriptionTextAlign="center"
+          slides={slides}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+        />
+       <Link href="/poses">
+          <button className="invisible  ">Back</button>
+        </Link>
+       
+      </div>
+      <Link href="/poses">
+          <button className="text-blue-500 text-2xl font-bold bg-black/20 rounded-b-xl px-4 pb-1">Back</button>
         </Link>
     </div>
   );
