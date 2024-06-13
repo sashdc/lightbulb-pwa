c59d1ae101c224fff2098b02784be4326dd76153
sashdc <saharshdc@gmail.com>
2024-06-13 15:52:35 -0400

    created gallery and lightbox page with favouriting functionality

--- app/family/page.js
+++ app/family/page.js
@@ -1,10 +1,40 @@
+'use client';
+
 import BackButton from "../ui/backbutton/backButtonFloat";
+import React, { useState } from 'react';
+import Lightbox from '../ui/lightbox/lightbox';
+import { engagementPhotos } from "@/public/images/engagement/engagement";
 
 export default function Family() {
+    const [lightboxOpen, setLightboxOpen] = useState(false);
+    const [photos, setPhotos] = useState(engagementPhotos);
+
+    const shufflePhotos = () => {
+        const shuffled = [...photos].sort(() => Math.random() - 0.5);
+        setPhotos(shuffled);
+    };
+
     return (
-        <div>
-        <div className="h-screen">Family</div>
+      <div className="h-full w-4/5 mx-auto">
         <BackButton backLocation="/poses" />
+        
+        <h1>Image Gallery</h1>
+        <div className="gallery animate-in fade-in-0">
+          {photos.map((photo, index) => (
+            <img
+              key={index}
+              src={photo.src}
+              alt={photo.alt}
+              onClick={() => setLightboxOpen(true)}
+              className="thumbnail animate-in zoom-in-50 "
+            />
+          ))}
         </div>
+        <Lightbox
+          images={photos}
+          isOpen={lightboxOpen}
+          onClose={() => setLightboxOpen(false)}
+        />
+      </div>
     );
 }
