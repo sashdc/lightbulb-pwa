e0c367647624e2653ff243f4a3f5b62d78a4f850
sashdc <saharshdc@gmail.com>
2024-06-18 13:23:25 -0400

    edited local storage retrieval on favourites

--- app/favourites/page.js
+++ app/favourites/page.js
@@ -1,14 +1,16 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Gallery from "../ui/galleryPage/galleryPage";
 
 export default function FavouritesGallery() {
     // Initialize favourites state with the stored favourites from localStorage
-    const [favourites, setFavourites] = useState(() => {
-        const storedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
-        return storedFavourites;
-    });
+    // const [favourites, setFavourites] = useState(() => {
+    //     const storedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
+    //     return storedFavourites;
+    // });
+
+    const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
 
   
     // useEffect(() => {
