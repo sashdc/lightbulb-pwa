deaad4fd86d58dfef7fbdc216e87559955de8a0a
sashdc <saharshdc@gmail.com>
2024-07-10 12:57:35 -0400

    added family photos

--- app/family/page.js
+++ app/family/page.js
@@ -1,8 +1,13 @@
+import Gallery from "../ui/galleryPage/galleryPage";
+import { familyPhotos } from "@/public/images/family/family";
+
 
 export default function Family() {
   return (
-  <div>
-    FAMILY POSES
-  </div>
+    <Gallery
+      photos={familyPhotos}
+      title="Family Poses"
+      backLink= "/poses"
+    />
   );
 }
